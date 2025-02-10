f0c46fb250c552ffd49e2232127137d21eaa24d9
Bruno2202 <brunoterribile2005@gmail.com>
2025-02-10 16:38:54 -0300

    :dizzy: Add loading animation when creating a room

--- src/components/modals/CreateChat.tsx
+++ src/components/modals/CreateChat.tsx
@@ -9,50 +9,42 @@ import ChatModel from "../../core/model/ChatModel";
 import { UserContext } from "../../contexts/UserContext";
 import { ChatController } from "../../core/controllers/ChatController";
 import { UserModel } from "../../core/model/UserModel";
+import { motion } from "motion/react"
 
 export default function CreateChat() {
     const [time, setTime] = useState<number>(3);
     const [chatLink, setChatLink] = useState<string>("");
-    const [newChat, setNewChat] = useState<ChatModel | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     const { userData, setUserData } = useContext(UserContext)!
     const { isOpenModal, closeModal } = useContext(ModalContext)!
 
     useEffect(() => {
-        if (isOpenModal("CreateChat") && userData && !newChat) {
-            const newChat = new ChatModel(
+        const handleCreateChat = async () => {
+            if (!isOpenModal("CreateChat") || !userData) return;
+
+            const createdChat = new ChatModel(
                 uuidv4().slice(0, 8),
                 userData.getId,
                 new Date()
             );
 
-            setNewChat(newChat);
+            setChatLink(`http://localhost:5173/${createdChat.getId}`);
 
-            const handleCreateChat = async () => {
-                if (newChat) {
-                    setChatLink(`http://localhost:5173/${newChat.getId}`);
+            try {
+                await ChatController.createChat(createdChat);
 
-                    await ChatController.createChat(newChat);
+                const updatedChats: ChatModel[] = await ChatController.getUserChats(createdChat.getOwnerId);
+                setUserData(new UserModel(userData.getId, userData.getUsername, updatedChats));
+            } catch (error) {
+                console.error("Erro ao criar chat:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
 
-                    try {
-                        const updatedChats: ChatModel[] = await ChatController.getUserChats(newChat.getOwnerId);
-                        if (userData) {
-                            const userDataUpdated = new UserModel(
-                                userData.getId,
-                                userData.getUsername,
-                                updatedChats
-                            );
-
-                            setUserData(userDataUpdated);
-                        }
-                    } catch (error) {
-                        console.error(error);
-                    }
-                }
-            };
-            handleCreateChat();
-        }
-    }, [isOpenModal, newChat]);
+        handleCreateChat();
+    }, [isOpenModal]);
 
     useEffect(() => {
         if (isOpenModal("CreateChat")) {
@@ -80,36 +72,46 @@ export default function CreateChat() {
             icon: "🔗"
         });
         closeModal("CreateChat");
+        setIsLoading(true);
         setTime(3);
-        setNewChat(null);
     }
 
     return (
         isOpenModal("CreateChat") && (
             <div className="flex items-center justify-center absolute w-full h-full">
                 <OpacityOverlay />
-                <div className="flex flex-col items-center relative w-auto h-auto p-4 bg-darkGrey rounded-8">
-                    <p className="text-white font-bold text-1.8rem  text-center">Um link para acesso à sala foi gerado!</p>
-                    <div className="flex flex-row items-center gap-2">
-                        <Link className="text-lightGrey" size={20} />
-                        <p className="text-lightGrey font-medium">Copie-o e envie para outra pessoa para iniciar a conversa</p>
-                    </div>
-                    <div className="flex flex-col gap-2 items-start w-full my-8">
-                        <div className="flex flex-row items-center gap-2">
-                            <CircleAlert className="text-orange" size={20} />
-                            <p className="text-sm text-orange font-medium">Garanta que o convite seja enviado à pessoa certa</p>
+                {isLoading ? (
+                    <motion.div layout className="flex flex-col items-center relative w-auto h-auto">
+                        <div className="flex flex-row text-white font-bold text-3rem text-center">
+                            <p className="animate-bounce">.</p>
+                            <p className="animate-bounce200">.</p>
+                            <p className="animate-bounce400">.</p>
                         </div>
+                    </motion.div >
+                ) : (
+                    <motion.div layout transition={{ duration: 0.4, ease: "circOut" }} className="flex flex-col items-center relative w-auto h-auto p-4 bg-darkGrey rounded-8">
+                        <p className="text-white font-bold text-1.8rem  text-center">Um link para acesso à sala foi gerado!</p>
                         <div className="flex flex-row items-center gap-2">
-                            <CircleAlert className="text-orange" size={20} />
-                            <p className="text-sm text-orange font-medium">Depois que outro usuário entrar, mais ninguém poderá acessar a conversa</p>
+                            <Link className="text-lightGrey" size={20} />
+                            <p className="text-lightGrey font-medium">Copie-o e envie para outra pessoa para iniciar a conversa</p>
                         </div>
-                    </div>
-                    <Button
-                        text={time === 0 ? "Copiar link" : time as unknown as string}
-                        onClick={() => handleCopyLink()}
-                        disabled={time === 0 ? false : true}
-                    />
-                </div>
+                        <div className="flex flex-col gap-2 items-start w-full my-8">
+                            <div className="flex flex-row items-center gap-2">
+                                <CircleAlert className="text-orange" size={20} />
+                                <p className="text-sm text-orange font-medium">Garanta que o convite seja enviado à pessoa certa</p>
+                            </div>
+                            <div className="flex flex-row items-center gap-2">
+                                <CircleAlert className="text-orange" size={20} />
+                                <p className="text-sm text-orange font-medium">Depois que outro usuário entrar, mais ninguém poderá acessar a conversa</p>
+                            </div>
+                        </div>
+                        <Button
+                            text={time === 0 ? "Copiar link" : time as unknown as string}
+                            onClick={() => handleCopyLink()}
+                            disabled={time === 0 ? false : true}
+                        />
+                    </motion.div >
+                )}
             </div >
         )
     );
