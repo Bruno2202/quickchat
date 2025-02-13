b7d9616b3a8cbfc2134e67eb18231a45c3891e1a
Bruno2202 <brunoterribile2005@gmail.com>
2025-02-13 19:49:54 -0300

    :sparkles: Add spin loader to Button for async onClick functions

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,26 @@
+import { LoaderCircle } from "lucide-react";
+
 interface Props {
     text: string;
     onClick?: () => void;
     disabled?: boolean;
+    isLoading?: boolean;
 }
 
-export default function Button({ text, onClick, disabled }: Props) {
-
+export default function Button({ text, onClick, disabled, isLoading }: Props) {
     return (
         <button
             className="p-2 bg-grey rounded-8 w-full text-white font-semibold hover:bg-blue btnAnimation transition-all"
             onClick={onClick}
             disabled={disabled}
         >
-            {text}
+            {isLoading ? (
+                <div className="flex items-center justify-center">
+                    <LoaderCircle className="animate-spin" />
+                </div>
+            ) : (
+                text
+            )}
         </button>
     )
 }
