a467d0a40b16381349c52aa0f467c266d9eab088
Bruno2202 <brunoterribile2005@gmail.com>
2025-02-13 19:45:55 -0300

    :sparkles: Add more props for input customization

--- src/components/inputs/Input.tsx
+++ src/components/inputs/Input.tsx
@@ -1,17 +1,30 @@
-interface Props {
-    maxLength: number;
-    type: string;
-    placeholder: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+import { DynamicIcon, IconName } from "lucide-react/dynamic";
+import { InputHTMLAttributes } from "react";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+    bgColor?: string;
+    color?: string;
+    icon?: IconName;
+    iconColor?: string;
+    placeholderColor?: string;
 }
 
-export default function Input({ maxLength, type, placeholder, onChange }: Props) {
+export default function Input({ bgColor, color, icon, iconColor, placeholderColor, ...props }: Props) {
     return (
-        <input
-            maxLength={maxLength}
-            onChange={onChange}
-            className="outline-none border border-1 rounded-8 p-2 placeholder:font-semibold"
-            type={type} placeholder={placeholder}
-        />
+        <div
+            className="flex flex-row items-center justify-center rounded-8"
+            style={{ backgroundColor: bgColor }}
+        >
+            {icon &&
+                <div className="flex items-center justify-center h-full px-2">
+                    <DynamicIcon color={iconColor} name={icon} />
+                </div>
+            }
+            <input
+                {...props}
+                style={{ backgroundColor: bgColor, color: color, WebkitTextFillColor: placeholderColor }}
+                className={`w-full outline-none ${icon ? "rounded-r-8 pr-2 py-2" : "rounded-8 p-2"} placeholder:font-semibold`}
+            />
+        </div>
     )
 }
